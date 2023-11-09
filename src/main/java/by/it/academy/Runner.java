eab97857b3ce65fd56e6d1c8b46d9dee5bb82743
Dzmitriy Stseba <Dzmitry.Stseba@solbeg.com>
2023-11-09 16:50:09 +0100

    TASK-1 new line

--- src/main/java/by/it/academy/Runner.java
+++ src/main/java/by/it/academy/Runner.java
@@ -4,5 +4,6 @@ public class Runner {
 public static void main( String[] args )
     {
         System.out.println( "Hello World! I am Dmitriy!!!!" );
+        System.out.println( "Hello World!" );
     }
 }
